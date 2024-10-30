1822d7f2303f6d0d6b86373e84bbad98bc4ac2a5
Fyo <fyosaadati@gmail.com>
2024-10-30 12:22:09 +0330

    refactor: print first x char

--- user_input.c
+++ user_input.c
@@ -1,9 +1,19 @@
-# include <stdio.h>
+#include <stdio.h>
 int main(){
-	char name[100];
-	printf("Enter your name:\n");
-	scanf("%s", &name);
-	printf("Hello %s !\n", name);
-	return 0;
+        char name[100];
+        char family[100];
+        printf("Enter your name:\n");
+        scanf("%s",name);
+        printf("Hello %s!\n", name);
+
+        /* Returns the first x characters of the entered value */
+        printf("Enter your last name:\n");
+        scanf("%3s", family);
+        printf("Only the first 3 characters of your input were printed: %s\n", family);
+        printf("You can change the number of characters as desired to print.\n");
+
+        /* scanf("%Xs", variable)
+           Replace X with the desired integer value */
+        return 0;
 }
-	
+
